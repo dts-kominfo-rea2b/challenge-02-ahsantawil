e8859d90d4bd9eef40e6694adf7092d457564872
ahsantawil <ahsan.tawil07@gmail.com>
2022-05-29 17:49:40 +0700

    Ahsan Ta'wil - Challenge Logic Flow

--- index.js
+++ index.js
@@ -23,19 +23,37 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+    for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+        const fullName = dataYangAkanDilooping[i].namaDepan + " " + dataYangAkanDilooping[i].namaBelakang;
+
+        hasilLooping[i] = fullName;
+
+    };
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
+    for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+      if (dataYangAkanDilooping[i].jenisKelamin == 'M') {
+        jumlahPria = jumlahPria + 1;
+      };
+      
+    };
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
+    for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+      if (dataYangAkanDilooping[i].jenisKelamin == 'F') {
+        jumlahWanita = jumlahWanita + 1;
+      };
+      
+    };
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,7 +66,14 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+    if (jumlahPria > jumlahWanita) {
+      komentar = "Jumlah Pria lebih banyak dari Wanita"
+    } else if (jumlahWanita > jumlahPria) {
+      komentar = "Jumlah Wanita lebih banyak dari Pria"
+    } else {
+      komentar = "Jumlah Pria dan Wanita Imbang"
+    }
 
   // ! JANGAN DIMODIFIKASI
   return {
@@ -65,6 +90,7 @@ function main(data) {
   console.log(hasil.hasilLooping);
   console.log(hasil.jumlahPria);
   console.log(hasil.jumlahWanita);
+  console.log(hasil.komentar);
 
   return hasil;
 }
